95429ce3b58aa18796de73651068efe14460661a
GRpro <rgrigoriy@mail.ua>
2015-03-14 18:50:48 +0200

    shop registration creation (1).

--- src/main/java/com/kpi/education/businesslogic/user/User.java
+++ src/main/java/com/kpi/education/businesslogic/user/User.java
@@ -18,6 +18,7 @@ public abstract class User {
 
     private String firstName;
     private String lastName;
+    
     @Column(unique = true)
     private String email;
     private String password;
